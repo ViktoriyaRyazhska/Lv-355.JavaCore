c5f9c67a5705fa986e850b91f8e1cfd73b68ad27
apcsmile <denysslobodian19@gmail.com>
2018-10-09 15:16:04 -0400

    Update firsttask.java

--- softServeFirstHomeTask/firsttask.java
+++ softServeFirstHomeTask/firsttask.java
@@ -7,12 +7,12 @@ import java.io.InputStreamReader;
 public class firsttask {
 public static void main(String[] args) throws IOException {
 	
-		double p = 3.14;
+		
 		BufferedReader mt = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter flower bed radius");
 		double r = Double.parseDouble(mt.readLine());
-		double perimetr = 2 * p * r;
-		double area = p * (r * r);
+		double perimetr = 2 * Math.PI * r;
+		double area = Math.PI * (r * r);
 		System.out.println("Perimetr = " + perimetr);
 		System.out.println("Area = " + area);
 }
