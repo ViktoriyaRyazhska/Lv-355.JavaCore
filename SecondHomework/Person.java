9cb3b7e68457c6c3fc2c86498e49be8801c96e08
Denys Slobodian <denysslobodian19@gmail.com>
2018-10-10 04:01:46 +0300

    done tasks

--- SecondHomework/Person.java
+++ SecondHomework/Person.java
@@ -1,12 +1,59 @@
 
-public class Person {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-	public static void main(String[] args) {
-		Details m1 = new Details();
-	m1.setName("Jake");
-	m1.setBirthYear(1986);
+public class Person {
+	private String name;
+	private int birthYear;
+	Calendar c = new GregorianCalendar();
 	
-	
+	public Person(String name, int birthYear) {
+		this.name = name;
+		this.birthYear = birthYear;
+	}
+
+	public Person() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public int age() {
+		return this.birthYear = c.get(Calendar.YEAR) - birthYear;
+		  
+	}
+
+	public void input(BufferedReader br) throws IOException {
+
+	System.out.println("Ваше імя?");
+		this.name = br.readLine();
+		System.out.println("Введіть рік народження");
+		this.birthYear = Integer.parseInt(br.readLine());
+		
 
 	}
+			
+	@Override
+
+	public String toString() {
+
+	return "Імя: " + name + ", Вік:" + age();
+
+}
 }
---
+++ SecondHomework/Program.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ * @author User
+ *
+ */
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		int i;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Person m1 = new Person();
+		m1.input(br);
+		
+		Person m2 = new Person();
+		m2.input(br);
+		
+		Person m3 = new Person();
+		m3.input(br);
+		Person m4 = new Person();
+		m4.input(br);
+		
+		Person m5 = new Person();
+		
+		m5.input(br);
+
+		//Object[] Person = new Person[5];
+		//Person [0] = new Person();
+			//	Person [1] = new Person();
+					//	Person [2] = new Person();
+					//			Person [3] = new Person();
+					//					Person [4] = new Person();
+		 
+	
+		
+		//for (i=0; i<5; i++) {
+		//	System.out.println(Person[i]);
+	//	}
+		System.out.println(m1);
+		System.out.println(m2);
+		System.out.println(m3);
+		System.out.println(m4);
+		System.out.println(m5);
+		
+	}
+
+}
