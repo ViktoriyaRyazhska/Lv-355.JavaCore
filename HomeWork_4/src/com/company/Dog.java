f76972f32ab30788ac613c990de252d10b45d2fd
onlyVone <43787744+onlyVone@users.noreply.github.com>
2018-10-15 17:49:37 +0300

    Change task2

--- HomeWork_4/src/com/company/Dog.java
+++ HomeWork_4/src/com/company/Dog.java
@@ -25,11 +25,11 @@ public class Dog {
 	}
 	
 	public static void nameDog(Dog d1, Dog d2, Dog d3) {
-		Dog[] mas = {d1, d2, d3};
+		Dog[] mas = {d1, d2, d3}; 
 		boolean y = false;
-		for(int i = 0; i < 3; i++) {			//перевірка собак на одне ім'я
+		for(int i = 0; i < mas.length; i++) {			//перевірка собак на одне ім'я
 			String newName = mas[i].getName();
-			for(int j=(i+1); j < 3;j++) {
+			for(int j=(i+1); j < mas.length;j++) {
 				if(newName.equals(mas[j].getName())) {
 					y = true;
 					System.out.println("dog#"+ (i+1)+ "--" + newName + " == " + mas[j].getName() + " -- dog#" + (j+1));
@@ -40,11 +40,11 @@ public class Dog {
 			System.out.println("Noone with the same name");
 		
 		int oldest = mas[0].getAge();  //найстаріша собака
-		for(int i = 1; i < 3; i++) {
+		for(int i = 1; i < mas.length; i++) {
 			if(mas[i].getAge() > oldest)
 				oldest = mas[i].getAge();
 		}
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < mas.length; i++) {
 			if(oldest == mas[i].getAge())
 				System.out.println("The oldest dog: " + mas[i].getName() + " " + mas[i].getBreed());
 		}
