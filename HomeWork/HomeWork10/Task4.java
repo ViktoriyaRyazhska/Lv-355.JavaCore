789b90cf45ac636bd01e0bf1df202643b22cb420
Andrii Shyian <andrew.shyian.87@gmail.com>
2018-11-05 21:59:23 +0200

    Task4 Fixed

--- HomeWork/HomeWork10/Task4.java
+++ HomeWork/HomeWork10/Task4.java
@@ -1,7 +1,4 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,14 +10,12 @@ public class Task4 {
 		String fileName = "1.txt";
 		FileReader f1 = new FileReader(fileName);
 		BufferedReader reader = new BufferedReader(f1);
-		FileOutputStream f = new FileOutputStream("2.txt");
 		Writer f2 = new FileWriter("2.txt");
 		String s;
 		ArrayList<String> list = new ArrayList<String>();
 		while ((s = reader.readLine()) != null) {
 			list.add(s);
 		}
-		System.out.println("Lines in your text: " + list.size());
 		int a = list.get(0).length();
 		int index = 0;
 		for (int i = 0; i < list.size(); i++) {
@@ -29,12 +24,10 @@ public class Task4 {
 				index = i;
 			}
 		}
-		System.out.println("The longest line: " + list.get(index) + a);
-		byte[] buffer = list.get(index).getBytes();
-		f.write(buffer, 0, buffer.length);
-		f2.write(list.size() + "");
+		f2.append(list.get(index));
 		f2.append("\n");
-		f2.close();
+		f2.write("Number of lines: "+list.size());
+		f2.append("\n");
 		String name = "";
 		String date = "";
 		for (int i = 0; i < list.size(); i++) {
@@ -43,15 +36,16 @@ public class Task4 {
 			}
 			if (list.get(i).contains("Date of birth")) {
 				date = list.get(i);
+				
 			}
 		}
-		byte[] buffer2 = name.getBytes();
-		f.write(buffer2, 0, buffer2.length);
-		byte[] buffer3 = date.getBytes();
-		f.write(buffer3, 0, buffer3.length);
-		System.out.println(name);
-		System.out.println(date);
+		
+		f2.append(name);
+		f2.append("\n");
+		f2.append(date);
+		f2.close();
 
 	}
 
 }
+
