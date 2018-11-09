fd48b407efdecf1260d3fa4a6f68ed4be9dae44b
onlyVone <43787744+onlyVone@users.noreply.github.com>
2018-11-10 01:40:29 +0200

    Project v1

--- weather/src/main/java/com/weather/weather.java
+++ weather/src/main/java/com/weather/weather.java
@@ -3,6 +3,9 @@ package com.weather;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,20 +13,88 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class weather {
-	
+
 	private static Document getPage() throws MalformedURLException, IOException {
 		String url = "https://sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BB%D1%8C%D0%B2%D0%BE%D0%B2";
 		Document page = Jsoup.parse(new URL(url), 3000);
 		return page;
 	}
+
+	private static Pattern pattern;
 	
+	private static String rebuildString(String s) {
+		s = s.replaceAll("\\s+", " ");
+		s = s.replaceAll(" :", ":");
+		return s;
+	}
 	
-	public static void main(String[] args) throws MalformedURLException, IOException {
+	private static String[] infoString(String s) {
+		int count = 0;
+		int i = 0;
+		String[] mas = new String[7];
+		String[] words = s.split(" ");
+		String value = "";
+		for (String word : words) {
+			if(count < 8) {
+				value += word + " ";
+				count++;
+			}if(count == 8) {
+				mas[i] = value;
+				i++;
+				count = 0;
+				value = "";
+			}
+		}
+		return mas;
+		
+	}
+	
+	// отримую дату(але неправильно)
+	private static String getYearFromString() throws Exception {
+		pattern = Pattern.compile("\\d{4}");
+		LocalDate localDate = LocalDate.now();
+		String ld = (localDate + "");
+		Matcher matcher = pattern.matcher(ld);
+		if (matcher.find()) {
+			return matcher.group();
+		}
+		throw new Exception("Can't extract date from string!");
+	}
+
+	private static void sample() throws Exception {
 		Document page = getPage();
 		Element tableWth = page.select("div#blockDays").first();
-		Elements mains = tableWth.select("div[class=main]");
-		System.out.println(mains);
-	
+//		System.out.println(tableWth);
+		Elements dates = tableWth.select("div.main");
+		String[] mas;
+		Elements partOfDay = tableWth.select("thead");
+
+		Elements infoValues = tableWth.select("tbody");
+
+
+		// основний вивід
+		for (Element day : dates) {
+			String date = day.select("p").text();
+			System.out.println(date + " " + getYearFromString()); // Суббота 10 ноября 2018
+
+			for (Element part : partOfDay) {
+				String parts = part.select("td").text();
+				System.out.println(parts);			//ночь утро день вечер
+				for (Element info : infoValues) {
+					String value = info.select("tr").text();
+					value = rebuildString(value);
+					mas = infoString(value);
+					for (String ss : mas) {
+						System.out.println(ss);//все остальные данные
+					}
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		sample();
 	}
 
 }
