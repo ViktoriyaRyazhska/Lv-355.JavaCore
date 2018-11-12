c8eeadf58e8132ce83fb06c711e6aa65b266bbcb
onlyVone <43787744+onlyVone@users.noreply.github.com>
2018-11-12 18:41:57 +0200

    Add 256

---
+++ FirstTeam/lastPracticalPart/src/lastPracticalPart/Main.java
@@ -0,0 +1,12 @@
+package lastPracticalPart;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Task_2.Decompose();
+		//Task_5.Polindrome();
+		//Task_6.CountOfWords();
+	}
+
+}
